541ed3a92b1c17b5d1ef726fc917819a8a2b57e3
LJuvo <www.704921698@qq.com>
2021-05-07 16:34:19 +0800

    init vue3 with vite typescript router axios less

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" /> -->
+  <router-view/>
+</template>
+
+<script lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+interface TempModel {
+  name: string;
+}
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+
+    }
+  },
+  mounted(){
+    this.initApp();
+  },
+  methods: {
+    initApp(): void {
+      let temp: TempModel = {
+        name: "TEMP ->"
+      };
+      console.log(temp);
+
+      this.$http.post('/dnalims/svc/loaddict', {}).then(function(res) {
+　　console.log(res)
+}.bind(this))
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+img {
+  width: 200px;
+}
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,54 @@
+import { createRouter, createWebHistory } from "vue-router";
+ 
+const routes = [
+  {
+    path: "/",
+    name: "about",
+    component: () => import("/@/views/about.vue"),
+  },
+  {
+    path: "/home",
+    name: "Home",
+    component: () => import("/@/views/home.vue"),
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("/@/views/login.vue"),
+  },
+];
+ 
+const router =  createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+router.beforeEach((to: any, from: any, next: any) => {
+   
+    //判断该用户有没有登录过
+    if(to.path === '/Login'){
+        if (localStorage.getItem('userInfo')) {
+            next({ path: '' })
+        }else{
+            next();
+        }
+    }else{
+        if (localStorage.getItem('userInfo')) {
+            if(to.path ==='/home/'){
+                next({ path: '/home' })
+            }else{
+                next()
+            }
+        }else{
+            next({ path: '/Login' })
+        }
+    }
+ 
+    
+})
+ 
+router.afterEach(() => {  
+    // 在即将进入新的页面组件前，关闭掉进度条
+})
+
+export default router;
